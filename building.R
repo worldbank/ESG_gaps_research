76806ce2b5ad293e3cae6503d4befd5e744db99e
Andres Castaneda <acastanedaa@worldbank.org>
2019-09-26 12:34:23 -0400

    Add file for building the book

---
+++ building.R
@@ -0,0 +1,15 @@
+
+# preview just one chapter
+file <- c("")
+bookdown::render_book(file, "bookdown::gitbook",
+                      preview = TRUE)
+beepr::beep(10)
+
+# render the whole book
+bookdown::render_book("index.Rmd", "bookdown::gitbook")
+beepr::beep(10)
+
+#serve the whole book to see modification on the fly
+dir <- getwd()
+bookdown::serve_book(dir = dir, output_dir = "docs",
+                     preview = TRUE)
