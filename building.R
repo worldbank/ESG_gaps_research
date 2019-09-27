c48f964ecc9d7d6913ae0c3c52af1ec05988c3cf
Andres Castaneda <acastanedaa@worldbank.org>
2019-09-27 12:39:09 -0400

    file to render book

--- building.R
+++ building.R
@@ -1,15 +1,17 @@
 
 # preview just one chapter
-file <- c("index.Rmd")
-bookdown::render_book(file, "bookdown::gitbook",
+file <- c("ESG_availability.Rmd")
+bookdown::render_book(file, "bookdown::tufte_html_book",
                       preview = TRUE)
 beepr::beep(10)
 
 # render the whole book
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
+bookdown::render_book("index.Rmd", "bookdown::tufte_html_book")
 beepr::beep(10)
 
 #serve the whole book to see modification on the fly
 dir <- getwd()
 bookdown::serve_book(dir = dir, output_dir = "docs",
                      preview = TRUE)
+
+
