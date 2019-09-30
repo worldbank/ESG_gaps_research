fd014aefce29e7e0d5aeace1df9019835921c493
Andres Castaneda <acastanedaa@worldbank.org>
2019-09-30 18:42:21 -0400

    msmb style

--- building.R
+++ building.R
@@ -1,13 +1,16 @@
 
 # preview just one chapter
 file <- c("ESG_hhsAvailability.Rmd")
+
 file <- c("ESG_volatility.Rmd")
-bookdown::render_book(file, "bookdown::tufte_html_book",
-                      preview = TRUE)
+bookdown::render_book(file, preview = TRUE)
+bfile <- paste0("docs/", sub("(.*)(\\.Rmd)", "\\1", file), ".html")
+browseURL(bfile)
 beepr::beep(10)
 
 # render the whole book
-bookdown::render_book("index.Rmd", "bookdown::tufte_html_book")
+bookdown::render_book("index.Rmd")
+browseURL("docs/index.html")
 beepr::beep(10)
 
 #serve the whole book to see modification on the fly
