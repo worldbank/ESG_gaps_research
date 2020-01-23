0ccd505531bd6c45f218864362dea7c49f6791c9
Tim Herzog <therzog1@worldbank.org>
2020-01-22 19:09:46 -0500

    Add extract_esg.py, which extracts ESG data from the API

---
+++ python/extract_esg.py
@@ -0,0 +1,29 @@
+
+# this script uses the WB API to extract all observations for the study period
+# Usage: python extract_esg.py FILE
+
+import csv
+import wbgapi as wb
+import sys
+
+reader = csv.reader(open(sys.argv[1], 'r'))
+writer = csv.writer(sys.stdout)
+
+writer.writerow(['db', 'series', 'economy', 'time', 'value'])
+
+for row in reader:
+    (cets,db) = row[0:2]
+    if not db.isnumeric():
+        continue # could be header or something not in the API
+
+    if int(db) == 11 or int(db) == 57:
+        continue    # archived databases
+
+    wb.db = db
+    try:
+        print('Fetching {} ({})'.format(cets, db), file=sys.stderr)
+
+        for elem in wb.data.fetch(cets, time=range(1990, 2020), skipAggs=True):
+            writer.writerow([db, elem['series'], elem['economy'], elem['time'], elem['value']])
+    except wb.APIError as err:
+        print('ERROR {} ({})'.format(cets, db), file=sys.stderr)
