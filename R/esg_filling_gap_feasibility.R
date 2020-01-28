ef605d27aab81a83478423b87b0f32165ecb6833
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-01-28 16:38:00 -0500

    update

--- R/esg_filling_gap_feasibility.R
+++ R/esg_filling_gap_feasibility.R
@@ -35,12 +35,16 @@ source("R/load_data.R")
 th_cg = .5       # threshold of coverage
 th_cv = .3       # Threshold of coefficient of variations
 tg_year = 2018   # target year
+mx_year = 2018   # Max year available option2: as.numeric(format(Sys.Date(), "%Y")) - 1
 
 
 # find max year per indicator/country
 x %>%
-  group_by(indicatorID, iso3c) %>%
-  summarise(mxy = max(date))  %>%  # max year avaiable
-  mutate()
+  group_by(indicatorID, indicator,  iso3c) %>%
+  summarise(mxy = max(date, na.rm = TRUE))  %>%  # max year avaiable
+  mutate(lst_y = if_else(mxy  %in% c(tg_year:mx_year), 1, 0)) %>% # is last year aviable?
+  group_by(indicatorID, indicator) %>%
+  summarise(lst_yd = mean(lst_y, na.rm = TRUE)) %>% # last year density
+  arrange(-lst_yd)
 
 
