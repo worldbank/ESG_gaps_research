780d47881d465e4b43f87479474c184661ef37f2
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-01-29 19:50:16 -0500

    add functions

--- R/esg_availability.R
+++ R/esg_availability.R
@@ -53,3 +53,48 @@ g1 <- ggplot(d1, aes( x = date,
 # make it interactive
 pg1 <- ggplotly(g1, tooltip = "text")
 
+
+
+
+#----------------------------------------------------------
+#   heatmap by explantion
+#----------------------------------------------------------
+
+
+d1a <- d1 %>%
+  filter(date >= 2000, date <= 2018) %>%
+  inner_join(select(mtd, cetsid, matches("^expl")),
+             by = c("indicatorID" = "cetsid"))
+
+
+hm_expl <- function(x, expl,
+                    label = NULL) {
+  expl <- enquo(expl)
+
+  if (length(label) > 0) {
+    labelf <-  paste0("Number of countries over time per indicator (",
+                     label, ")")
+  } else {
+    labelf <- "Number of countries over time per indicator"
+  }
+
+  g1a <- ggplot(data = filter(x, !!expl == 1),
+                aes(text = text)) +
+    geom_tile(aes( x = date,
+                   y = ind,
+                   fill = n)) +
+    scale_fill_distiller(palette = "Spectral",
+                         direction = 1) +
+    labs(x = "", y = "") +
+    scale_x_continuous(breaks = c(2000:2018),
+                       expand = c(0,0)) +
+    theme(axis.text.x = element_text(size = rel(0.8),
+                                     angle = 330,
+                                     hjust = 0,
+                                     colour = "grey50"),
+          axis.text.y = element_text(size = rel(0.5),
+                                     colour = "grey50")) +
+    ggtitle(label = labelf)
+  ggplotly(g1a, tooltip = "text")
+}
+
