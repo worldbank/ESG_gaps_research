3d3339e760e12ee80fd8ab936289f325fec59cb2
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-02-03 10:29:29 -0500

    update chapter  4

--- building.R
+++ building.R
@@ -7,9 +7,11 @@
 #
 # file <- c("ESG_availability.Rmd")
 # file <- c("ESG_resultsbyexplanation.Rmd")
-#  file <- c("ESG_background.Rmd")
-#  bookdown::render_book(file, preview = TRUE)
-#  beepr::beep(10)
+# file <- c("ESG_variability.Rmd")
+# bookdown::render_book(file, preview = TRUE)
+# beepr::beep(10)
+# browseURL("docs/volatility.html")
+#
 # browseURL("docs/background.html")
 
 # bfile <- paste0("docs/", sub("(.*)(\\.Rmd)", "\\1", file), ".html")
