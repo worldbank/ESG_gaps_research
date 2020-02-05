6ad52b15456233348ab9465a09086dc2245c1ec8
Tim Herzog <therzog1@worldbank.org>
2020-02-05 17:39:25 -0500

    Add iso2 to extract script

--- python/extract_esg.py
+++ python/extract_esg.py
@@ -16,7 +16,7 @@ economy_filter = [row['id'] for row in wb.economy.list(skipAggs=True)]
 time_range = range(1990, 2051)
 
 # writer.writerow(['db', 'series', 'economy', 'time', 'value'])
-writer.writerow(['db', 'iso3c', 'date', 'value', 'indicatorID', 'indicator', 'country'])
+writer.writerow(['db', 'iso3c', 'date', 'value', 'indicatorID', 'indicator', 'iso2c', 'country'])
 
 for row in reader:
     (cets,db) = row[0:2]
@@ -30,10 +30,16 @@ for row in reader:
     try:
         print('Fetching {} ({})'.format(cets, db), file=sys.stderr)
 
-        for elem in wb.data.fetch(cets, time=time_range, skipAggs=True, labels=True):
+        for elem in wb.data.fetch(cets, time=time_range, skipAggs=True, skipBlanks=True, labels=True):
             if elem['economy']['id'] in economy_filter:
                 # writer.writerow([db, elem['series'], elem['economy'], elem['time'], elem['value']])
-                writer.writerow([db, elem['economy']['id'], elem['time']['value'], elem['value'], elem['series']['id'], elem['series']['value'], elem['economy']['value']])
+                writer.writerow([db, elem['economy']['id'],
+                    elem['time']['value'],
+                    elem['value'],
+                    elem['series']['id'],
+                    elem['series']['value'],
+                    wb.economy.iso2(elem['economy']['id']),
+                    elem['economy']['value']])
     except wb.APIError as err:
         print('ERROR {} ({})'.format(cets, db), file=sys.stderr)
         print(err, file=sys.stderr)
